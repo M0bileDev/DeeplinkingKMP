1dd6da268a8e7a393c156917b9a406710fcf9080
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-28 23:53:49 +0100

    feat(navigation): provide implementation of the detail screen

--- composeApp/src/commonMain/kotlin/com/example/deeplinkingkmp/navigation/NavRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/deeplinkingkmp/navigation/NavRoot.kt
@@ -1,6 +1,7 @@
 package com.example.deeplinkingkmp.navigation
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,11 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 
 @Composable
 fun NavRoot() {
@@ -36,6 +39,15 @@ fun NavRoot() {
                 }
             }
         }
-        composable<Route.Details> { }
+        composable<Route.Details> { backStackEntry ->
+            val route = backStackEntry.toRoute<Route.Details>()
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Item: ${route.id}"
+                )
+            }
+        }
     }
 }
