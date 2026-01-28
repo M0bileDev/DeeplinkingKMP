23130d663ed715d3904820ec73de4be1bb06d334
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-28 23:51:17 +0100

    feat(navigation): provide implementation of the list screen

--- composeApp/src/commonMain/kotlin/com/example/deeplinkingkmp/navigation/NavRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/deeplinkingkmp/navigation/NavRoot.kt
@@ -1,18 +1,41 @@
 package com.example.deeplinkingkmp.navigation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun NavRoot(){
+fun NavRoot() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = Route.List
-    ){
-        composable<Route.List> {  }
-        composable<Route.Details> {  }
+    ) {
+        composable<Route.List> {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(100) { item ->
+                    Text(
+                        modifier = Modifier.fillMaxWidth().padding(16.dp).clickable {
+                            navController.navigate(Route.Details(item))
+                        },
+                        text = "Item: $item"
+                    )
+                }
+            }
+        }
+        composable<Route.Details> { }
     }
 }
