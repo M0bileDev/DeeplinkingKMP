feb2402a48e15f3e4a9fd4edb18a1c0689b7c261
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-29 23:35:12 +0100

    feat(iosApp): provide external uri handler

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
@@ -6,7 +7,9 @@ struct iOSApp: App {
         WindowGroup {
             ContentView()
                 .onOpenURL(perform: {url in
+                    //open ios devtools and type -> xcrun simctl openurl booted "myapp://deep-link.com/item/15"
                     //callback lambda that ios will call when open with specific url
+                    ExternalUriHandler.shared.onNewUri(uri: url.absoluteString)
                 })
         }
     }
